1200e09b55d1013760ef3d9d04e48d850557df7f
Diego Eloy <89320699+diegoesilva@users.noreply.github.com>
2022-07-31 18:11:12 -0300

    Add files via upload

---
+++ Try catch.py
@@ -0,0 +1,12 @@
+# Crie uma situação em que ocorra uma exceção dentro de um código.
+# Utilize try/catch para realizar a captura e tratamento dessa exceção.
+
+try :
+
+    print(4/0)
+
+except ZeroDivisionError as erro:
+
+    print("Erro:", erro)
+
+    
